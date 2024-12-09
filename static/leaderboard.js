5dbcac2c63f27e410c55da810b011b7420c417dd
soehlj <soehlj@oregonstate.edu>
2024-12-08 23:29:27 -0800

    Added code

---
+++ static/leaderboard.js
@@ -0,0 +1,17 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['leaderboard'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return " <li class = \"leaderboard names\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":1,"column":33},"end":{"line":1,"column":41}}}) : helper)))
+    + " = "
+    + alias4(((helper = (helper = lookupProperty(helpers,"wins") || (depth0 != null ? lookupProperty(depth0,"wins") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"wins","hash":{},"data":data,"loc":{"start":{"line":1,"column":44},"end":{"line":1,"column":52}}}) : helper)))
+    + " wins</li> ";
+},"useData":true});
+})();
